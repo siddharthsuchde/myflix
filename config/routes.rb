7e7b7e8df548a908bbd65658e65af528a6a05757
siddharthsuchde <siddharthsuchde@gmail.com>
2015-01-09 02:38:52 +0000

    Precourse Changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Myflix::Application.routes.draw do
+  
+  root to: 'videos#index'
   get 'ui(/:action)', controller: 'ui'
+  
+  resources :videos, only: [:show] do
+    resources :categories, only: [:index]
+  end
+  
+  resources :categories, only: [:show]
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.create(title: "family_guy", description: "This is a great video", small_cover_url: "family_guy")
+Video.create(title: "futurama", description: "This is average", small_cover_url: "futurama" )
+Video.create(title: "monk", description: "not highly rate", small_cover_url: "monk" )
+
