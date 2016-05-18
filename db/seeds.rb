a4ddfbd98af5c99dfe2aaac928b33fa7e148a3d9
Siddharth <siddharth@liveyoursport.com>
2016-05-18 04:18:26 +0000

    Weed 6 changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-load(Rails.root.join("db", "seeds.rb"))
+
 
 comedies = Category.create(name: 'Comedies')
 drama = Category.create(name: 'Drama')
