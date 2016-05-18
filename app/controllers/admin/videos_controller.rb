c47fc53a755834d9a94717cd368ac7633be37ecc
Siddharth <siddharth@liveyoursport.com>
2016-05-18 04:14:57 +0000

    Week 6 changes

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -29,7 +29,7 @@ class Admin::VideosController < ApplicationController
   end
   
   def video_params
-    params.require(:video).permit(:title, :description, :category_id)
+    params.require(:video).permit(:title, :description, :category_id, :small_cover, :large_cover, :video_url)
   end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+load(Rails.root.join("db", "seeds.rb"))
+
 comedies = Category.create(name: 'Comedies')
 drama = Category.create(name: 'Drama')
 romance = Category.create(name: 'Romance')
 
-Video.create(title: "family_guy", description: "This is a great video", small_cover_url: "family_guy", category: comedies)
+Video.create(title: "family_guy", description: "This is a great video", small_cover: File.open(File.join(Rails.root, '/tmp/family_guy.jpg')), large_cover: File.open(File.join(Rails.root, '/tmp/family_guy.jpg')), video_url: 'https://diikjwpmj92eg.cloudfront.net/uploads/week6/HW3%20watch%20video.mp4' category: comedies)
 Video.create(title: "futurama", description: "This is average", small_cover_url: "futurama", category: comedies )
 Video.create(title: "monk", description: "not highly rate", small_cover_url: "monk", category: drama)
 Video.create(title: "south_park", description: "Adult comdey", small_cover_url: "south_park", category: romance)
